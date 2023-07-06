8c7f5fdab908fe98040d843a43f8f8123cbb11df
Tunliz <tunlizstakes2004@gmail.com>
2023-07-06 10:58:16 -0500

    script update
    script update

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ const submit = document.getElementsByClassName('form-contact')[0];
 submit.addEventListener('submit',(e)=>{
   e.preventDefault();
   console.log("Clicked");
+
+  //Email Code Here....
+  Email.send({
+    SecureToken : "e6f1ab25-65e-4234-899b-e81047531693",
+    To : 'oyesijibaasit04@gmail.com',
+    From : "tunlizstakes@gmail.com",
+    Subject : "Testing my mail App",
+    Body : "Testing my mailing app i hope it works.."
+}).then(
+  message => alert(message)
+);
 })
